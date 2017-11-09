01125f4f76888991c52f134bda639c746185a8f8
yjsung07@gmail.com <syj0613>
2017-11-09 17:06:55 +0900

    clipart 이용한 시나리오 완성

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -4,6 +4,7 @@
 #include <android/log.h>
 #include <android/asset_manager_jni.h>
 #include <string.h>
+#include <stdlib.h>
 
 using namespace cv;
 using namespace std;
@@ -11,7 +12,68 @@ using namespace std;
 extern "C" {
 
 JNIEXPORT void JNICALL
-Java_software_happybubble_ImageProcessingActivity_imageProcessing(
+Java_software_happybubble_PictureProcessing_imageProcessing(
+        JNIEnv *env,
+        jobject,
+        jlong addrInputImage,
+        jlong addrBinaryOutputImage,
+        jlong addrColorOutputImage,
+        jfloat h,
+        jfloat s,
+        jfloat v) {
+
+    Mat &img_input = *(Mat *) addrInputImage;
+    Mat &img_binaryOutput = *(Mat *) addrBinaryOutputImage;
+    Mat &img_colorOutput = *(Mat *) addrColorOutputImage;
+    Mat temp_mask;
+    float min[3], max[3];
+    cvtColor(img_input, img_binaryOutput, CV_BGR2RGB);
+
+    if(h > 225) {
+        min[0] = h - 30;
+        max[0] = 255;
+    }
+    else if(h < 30) {
+        min[0] = 0;
+        max[0] = h + 30;
+    }
+    else {
+        min[0] = h - 30;
+        max[0] = h + 30;
+    }
+
+    if(s > 225) {
+        min[1] = s - 30;
+        max[1] = 255;
+    }
+    else if(s < 30) {
+        min[1] = 0;
+        max[1] = s + 30;
+    }
+    else {
+        min[1] = s - 30;
+        max[1] = s + 30;
+    }
+
+    if(v > 225) {
+        min[2] = v - 30;
+        max[2] = 255;
+    }
+    else if(v < 30) {
+        min[2] = 0;
+        max[2] = v + 30;
+    }
+    else {
+        min[2] = v - 30;
+        max[2] = v + 30;
+    }
+    inRange(img_binaryOutput, Scalar(min[2], min[1], min[0]), Scalar(max[2], max[1], max[0]), temp_mask);
+
+    img_input.copyTo(img_colorOutput, temp_mask);
+}
+
+JNIEXPORT void JNICALL
+ava_software_happybubble_ImageProcessingActivity_imageProcessing(
         JNIEnv *env,
         jobject,
         jlong addrInputImage,
@@ -23,32 +85,20 @@ Java_software_happybubble_ImageProcessingActivity_imageProcessing(
     Mat temp_mask;
 
     cvtColor(img_input, img_output, CV_BGR2HSV);
-
-    __android_log_print(ANDROID_LOG_ERROR, "COLOR", "%d", color);
-
-    if(color == 0)
-        inRange(img_output, Scalar(100, 100, 100), Scalar(120, 255, 255), temp_mask);
-    else if(color == 1)
-        inRange(img_output, Scalar(70, 100, 100), Scalar(90, 255, 255), temp_mask);
-    else if(color == 2)
-        inRange(img_output, Scalar(0, 100, 100), Scalar(20, 255, 255), temp_mask);
-    else if(color == 3)
-        inRange(img_output, Scalar(0, 0, 100), Scalar(0, 0, 255), temp_mask);
-
-    img_input.copyTo(img_output, temp_mask);
 }
 
-JNIEXPORT void JNICALL
+JNIEXPORT jstring JNICALL
 Java_software_happybubble_ImageProcessingActivity_imageLableing(
         JNIEnv *env,
-        jobject,
+        jobject obj,
         jlong addrInputImage,
-        jlong addrOutputImage) {
-
+        jlong addrStatsImage,
+        jlong addrCentroidsImage) {
     Mat &img_input = *(Mat *) addrInputImage;
-    Mat &img_output = *(Mat *) addrOutputImage;
+    Mat &stats = *(Mat *) addrStatsImage;
+    Mat &centroids = *(Mat *) addrStatsImage;
+    Mat statsTemp, centroidsTemp;
     Mat binary_output, erode_output, dilate_output, labeling_output;
-    Mat stats, centroids;
     Mat mask = getStructuringElement(MORPH_RECT, Size(3, 3), Point(1, 1));
 
     cvtColor(img_input, binary_output, CV_RGB2GRAY);
@@ -62,16 +112,55 @@ Java_software_happybubble_ImageProcessingActivity_imageLableing(
     erode(binary_output, erode_output, mask, Point(-1, -1), 3);
     // dilate = 팽창 연산
     dilate(erode_output, dilate_output, mask, Point(-1, -1), 1);
-    dilate(binary_output, binary_output, mask, Point(-1, -1), 5);
-    for(int h = 0;h < height;h++)
-        for(int w = 0;w < width;w++)
-            morphology_output.at<uchar>(h,w) = abs(binary_output.at<uchar>(h,w) - dilate_output.at<uchar>(h,w));
+    dilate(binary_output, binary_output, mask, Point(-1, -1), 1);
+    for (int h = 0; h < height; h++)
+        for (int w = 0; w < width; w++)
+            morphology_output.at<uchar>(h, w) = abs(
+                    binary_output.at<uchar>(h, w) - dilate_output.at<uchar>(h, w));
 
-    int numOfLables = connectedComponentsWithStats(morphology_output, labeling_output, stats, centroids, 8,CV_32S);
+    int numOfLables = connectedComponentsWithStats(morphology_output, labeling_output, statsTemp, centroidsTemp, 8, CV_32S);
 
-    morphology_output.copyTo(img_output);
-    cvtColor(img_output, img_output, CV_GRAY2RGB);
+    char *info = "";
+    char *temp = "1";
+    for (int j = 1; j < numOfLables; j++) {
+        int area = statsTemp.at<int>(j, CC_STAT_AREA);
+        int left = statsTemp.at<int>(j, CC_STAT_LEFT);
+        int top = statsTemp.at<int>(j, CC_STAT_TOP);
+        int width = statsTemp.at<int>(j, CC_STAT_WIDTH);
+        int height = statsTemp.at<int>(j, CC_STAT_HEIGHT);
 
+        int x = centroidsTemp.at<double>(j, 0); //중심좌표
+        int y = centroidsTemp.at<double>(j, 1);
+
+        if(width > 200 && height > 200){
+            //sprintf(info,"%d,%d,%d,%d,%d,%d,%d",area,left,top,width,height,x,y);
+            strcat(info, temp);
+        }
+    }
+    jstring result;
+    result = env->NewStringUTF("test");
+    return result;
+}
+
+JNIEXPORT void JNICALL
+Java_software_happybubble_ImageProcessingActivity_getLableingImg(
+        JNIEnv *env,
+        jobject obj,
+        jint numOfLables,
+        jint returnLable,
+        jlong addrInputImage,
+        jlong addrOutputImage,
+        jlong addrStatsImage,
+        jlong addrCentroidsImage) {
+    Mat &img_input = *(Mat *) addrInputImage;
+    Mat &img_output = *(Mat *) addrOutputImage;
+    Mat &stats = *(Mat *) addrStatsImage;
+    Mat &centroids = *(Mat *) addrCentroidsImage;
+    int lableFilter = 0;
+
+    printf("return : %d\n", returnLable);
+    printf("lable : %d\n", lableFilter);
+    //morphology_output.copyTo(img_input);
     for (int j = 1; j < numOfLables; j++) {
         int area = stats.at<int>(j, CC_STAT_AREA);
         int left = stats.at<int>(j, CC_STAT_LEFT);
@@ -82,9 +171,15 @@ Java_software_happybubble_ImageProcessingActivity_imageLableing(
         int x = centroids.at<double>(j, 0); //중심좌표
         int y = centroids.at<double>(j, 1);
 
-        if(width > 20 || height > 20){
-            circle(img_output, Point(x, y), 5, Scalar(0, 255, 0), 1);
-            rectangle(img_output, Point(left, top), Point(left + width, top + height), Scalar(255, 0, 0), 5);
+        if(width > 200 && height > 200){
+            circle(img_input, Point(x, y), 5, Scalar(0, 255, 0), 1);
+            rectangle(img_input, Point(left, top), Point(left + width, top + height), Scalar(255, 0, 0), 5);
+            //lableingImg[result++].create(height, width, CV_8UC1);
+            Rect roi(left, top, width, height);
+            img_output = img_input(roi).clone();
+            //env->CallVoidMethod(obj, cb, env->NewStringUTF("Lable"));
+            if(returnLable == lableFilter)  break;
+            lableFilter++;
         }
     }
 }
--- app/src/main/java/software/happybubble/ImageProcessingActivity.java
+++ app/src/main/java/software/happybubble/ImageProcessingActivity.java
@@ -1,12 +1,12 @@
 package software.happybubble;
 
+import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
-import android.graphics.Matrix;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -17,6 +17,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
@@ -26,6 +28,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 
 public class ImageProcessingActivity extends AppCompatActivity {
 
@@ -35,15 +38,21 @@ public class ImageProcessingActivity extends AppCompatActivity {
     }
 
     ImageView imageVIewInput;
-    ImageView imageVIewOuput;
-    Button camera, album, doitR, doitY, doitB, doitW;
-    private Mat img_input;
-    private Mat img_output;
+    ImageView imageVIewOutput;
+    Button camera, album, lable, doitR, doitY, doitB, doitW;
+    public static ItemListViewAdapter lableingListAdapter;
+    public static ItemListView lableingList;
+    ArrayList<ListView> list = new ArrayList<ListView>();
+    ArrayList<Bitmap> img = new ArrayList<Bitmap>();
+    ArrayList<String> txt = new ArrayList<String>();
+    private View view;
+    private Activity activity;
+    private Mat img_input, img_output, img_stats, img_centroids;
     static final int REQUEST_CAMERA = 1;
     static final int REQUEST_ALBUM = 2;
-
     private static final String TAG = "opencv";
     static final int PERMISSION_REQUEST_CODE = 1;
+    static int index = 0;
     String[] PERMISSIONS  = {"android.permission.WRITE_EXTERNAL_STORAGE"};
 
     private boolean hasPermissions(String[] permissions) {
@@ -148,12 +157,14 @@ public class ImageProcessingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_imageprocessing);
+        setContentView(R.layout.activity_image_processing);
+        activity = this;
 
         imageVIewInput = (ImageView)findViewById(R.id.imageViewInput);
-        imageVIewOuput = (ImageView)findViewById(R.id.imageViewOutput);
+        imageVIewOutput = (ImageView)findViewById(R.id.imageViewOutput);
         camera = (Button)findViewById(R.id.camera);
         album = (Button)findViewById(R.id.album);
+        lable = (Button)findViewById(R.id.lable);
         doitR = (Button)findViewById(R.id.doitR);
         doitY = (Button)findViewById(R.id.doitY);
         doitB = (Button)findViewById(R.id.doitB);
@@ -175,26 +186,93 @@ public class ImageProcessingActivity extends AppCompatActivity {
                     doTakeAlbumAction();
                 }
             });
+            lable.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(getApplicationContext(),"start",Toast.LENGTH_SHORT).show();
+                    String numberOfLables = imageLableing(img_input.getNativeObjAddr(), img_stats.getNativeObjAddr(), img_centroids.getNativeObjAddr());
+                    /*
+                    Bitmap bitmapInput = Bitmap.createBitmap(img_input.cols(), img_input.rows(), Bitmap.Config.ARGB_8888);
+                    Bitmap bitmapOutput;
+                    Utils.matToBitmap(img_input, bitmapInput);
+                    imageVIewInput.setImageBitmap(bitmapInput);
+*/
+                    Toast.makeText(getApplicationContext(),numberOfLables,Toast.LENGTH_SHORT).show();
+/*
+                    for(int i = 0;i < numberOfLables;i++) {
+                        getLableingImg(numberOfLables, i, img_input.getNativeObjAddr(), img_output.getNativeObjAddr(), img_stats.getNativeObjAddr(), img_centroids.getNativeObjAddr());
+                        bitmapOutput = Bitmap.createBitmap(img_output.cols(), img_output.rows(), Bitmap.Config.ARGB_8888);
+                        Utils.matToBitmap(img_output, bitmapOutput);
+                        img.add(bitmapOutput);
+                        Toast.makeText(getApplicationContext(), ""+numberOfLables, Toast.LENGTH_SHORT).show();
+                    }
+
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(activity);
+                    alertBuilder.setTitle("항목중에 하나를 선택하세요.");
+
+                    lableingList = (ItemListView) findViewById(R.id.lableList);
+                    lableingListAdapter = new ItemListViewAdapter(activity, R.layout.activity_select_clipart, list);
+                    for(int i = 0; i < img.size(); i++){
+                        list.add(new ItemListView(img.get(i)));
+                    }
+                    lableingList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                            Log.d("test",""+position);
+                            imageVIewOutput.setImageBitmap(img.get(position));
+                            Toast.makeText(getApplicationContext(),txt.get(position),Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                    alertBuilder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog,int which) { dialog.dismiss(); }
+                    });
+
+                    alertBuilder.setAdapter(lableingListAdapter,
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,
+                                                    int id) {
+
+                                    long strName = lableingListAdapter.getItemId(id);
+                                    AlertDialog.Builder innBuilder = new AlertDialog.Builder(activity);
+                                    innBuilder.setMessage("" + strName);
+                                    innBuilder.setTitle("당신이 선택한 것은 ");
+                                    innBuilder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            dialog.dismiss();
+                                        }
+                                    });
+                                    innBuilder.show();
+                                }
+                            });
+                    alertBuilder.show();*/
+                }
+            });
             doitR.setOnClickListener(new View.OnClickListener(){
                 @Override
-                public void onClick(View view) { imageprocess_and_showResult(0);
+                public void onClick(View view) {
+                    imageprocess_and_showResult(0);
                 }
             });
             doitY.setOnClickListener(new View.OnClickListener(){
                 @Override
-                public void onClick(View view) { imageprocess_and_showResult(1);
+                public void onClick(View view) {
+                    imageprocess_and_showResult(1);
                 }
             });
             doitB.setOnClickListener(new View.OnClickListener(){
                 @Override
-                public void onClick(View view) { imageprocess_and_showResult(2);
+                public void onClick(View view) {
+                    imageprocess_and_showResult(2);
                 }
             });
             doitW.setOnClickListener(new View.OnClickListener(){
                 @Override
-                public void onClick(View view) { imageprocess_and_showResult(3);
+                public void onClick(View view) {
+                    imageprocess_and_showResult(3);
                 }
             });
+
         }
     }
 
@@ -202,12 +280,14 @@ public class ImageProcessingActivity extends AppCompatActivity {
         imageProcessing(img_input.getNativeObjAddr(), img_output.getNativeObjAddr(), color);
         Bitmap bitmapOutput = Bitmap.createBitmap(img_output.cols(), img_output.rows(), Bitmap.Config.ARGB_8888);
         Utils.matToBitmap(img_output, bitmapOutput);
-        imageVIewOuput.setImageBitmap(bitmapOutput);
+        imageVIewOutput.setImageBitmap(bitmapOutput);
     }
 
     private void read_image_file() {
         img_input = new Mat();
         img_output = new Mat();
+        img_stats = new Mat();
+        img_centroids = new Mat();
     }
 
     private void doTakePhotoAction() {
@@ -271,6 +351,7 @@ public class ImageProcessingActivity extends AppCompatActivity {
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
      */
-    public native void imageLableing(long inputImage, long outputImage, int color);
+    public native String imageLableing(long inputImage, long statsImage, long centroidsImage);
+    public native void getLableingImg(int numOfLables, int returnLable, long addrInputImage, long addrOutputImage,long addrStatsImage, long addrCentroidsImage);
     public native void imageProcessing(long inputImage, long outputImage, int color);
 }
